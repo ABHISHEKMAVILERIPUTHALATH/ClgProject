62927c8e0b9c9f9ab69c3dd33aaa752121557a03
abhishek <abhishekgireesh2002@gmail.com>
2023-06-25 22:12:50 +0530

    submission form

--- main/Order/order.js
+++ main/Order/order.js
@@ -24,7 +24,7 @@ var db = firebase.firestore();
                         <p>${data.count}X ${data.item}</p>
                         <p>${data.count}X${data.price}</p>
                         <p>total price ${data.count*data.price}</p>
-                        <button id="ProdReady">Ready</button>
+                        <button id="ProdReady" onclick="deleteorder('${doc.id}')">Ready</button>
                     </div>
                 </div>
                 `
@@ -36,26 +36,56 @@ var db = firebase.firestore();
                 //     findiv.textContent=final;
                 //     docfin.appendChild(findiv);
                 // }
-
+                
+                // console.log(count)
+                var total=[]
                 // Function to fetch and render all documents in the collection
                 function fetchAndRenderDocument() {
+                    total[0]=0
+                    total[1]=0
+                    total[2]=null
+                    
                     db.collection('orders') // Replace "your-collection-name" with the actual collection name
                     .get()
                     .then(function(querySnapshot) {
                         querySnapshot.forEach(function(doc) {
                             renderDocumen(doc);
+                            total[0]+=doc.data().count*doc.data().price;
+                            total[1]+=parseInt(doc.data().count);
+                            total[2]+=1
+                            
+                            console.log(total[2]);
+                            document.querySelector(".prepno").innerHTML=total[2]
+                            document.getElementById('totalorder').innerHTML="Total order:"+ total[1]
+                            document.getElementById('earning').innerHTML="Total earning:"+ total[0]
+                            
                             // let d=doc.data()
                             // let k=`<div>${d.item}</div>`;
                             // var final;
-                        // final+=k
-                        
-                    }
-                    ) ;       //pas(final);
+                            // final+=k
+                            
+                        }
+                        ) ;       //pas(final);
                     })
                     .catch(function(error) {
-                    console.error('Error fetching documents: ', error);
+                        console.error('Error fetching documents: ', error);
                     });
-                    }
+                }
+                console.log(total[0]);
 
                     // Call the function to fetch and render the documents when needed
                     fetchAndRenderDocument();
+                    var count=null
+                    function deleteorder(id){
+                        count+=1
+                        document.querySelector(".redno").innerHTML=count;
+                        document.getElementById('totaluser').innerHTML="Total no of users:"+ count;
+                        console.log(id);
+                        db.collection("orders").doc(id).delete().then(() => {
+                            console.log("Document successfully deleted!");
+                            orderlist.innerHTML="";
+                            fetchAndRenderDocument();
+                        }).catch((error) => {
+                            console.error("Error removing document: ", error);
+                        });
+                    }
